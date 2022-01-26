ff49b46d3fb8d589f0e93d0bb05d35f18836ff69
sreejit-chatterjee <68802464+sreejit-chatterjee@users.noreply.github.com>
2022-01-26 10:12:09 +0530

    NavBar created

--- edugo/src/App.js
+++ edugo/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
 import React, { useState } from "react";
 
 function App() {
   return (
-    
+    <NavBar/>
   );
 }
 
